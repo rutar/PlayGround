6fcc022274793bac0c8acdc53025e1e4a5d16d0b
Ruslan Targonski <rustarg@gmail.com>
2023-05-23 22:17:19 +0300

    remove playSiteFactory

--- src/main/java/com/example/playground/service/PlayGroundService.java
+++ src/main/java/com/example/playground/service/PlayGroundService.java
@@ -1,7 +1,6 @@
 package com.example.playground.service;
 
 import com.example.playground.configuration.AppConfig;
-import com.example.playground.configuration.PlaySiteFactory;
 import com.example.playground.model.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
@@ -13,14 +12,10 @@ public class PlayGroundService {
     AnnotationConfigApplicationContext context
             = new AnnotationConfigApplicationContext(AppConfig.class);
     private final PlayGround playGround = new PlayGround();
-    private final PlaySiteFactory playSiteFactory = new PlaySiteFactory();
 
     public PlaySite addPlaySite(final String playSiteType) {
 
-        // there are two ways of PlaySite installation from app context of from play site factory
-
-        //PlaySite playSiteToAdd = (PlaySite)context.getBean(playSiteType);
-        PlaySite playSiteToAdd = this.playSiteFactory.getPlaySite(playSiteType);
+        PlaySite playSiteToAdd = (PlaySite)context.getBean(playSiteType);
 
         this.playGround.addPlaySite(playSiteToAdd);
         return playSiteToAdd;
